af55861f3fc268aeb7e6b6821fcef387f48f9f8f
AdyC24 <adychandra101@gmail.com>
2024-08-16 04:10:35 +0800

    built[react]: bangun frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-blue-500 min-h-screen flex items-center justify-center text-white">
+        <h1 className="text-6xl font-bold">
+          Welcome to React with Tailwind CSS!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
