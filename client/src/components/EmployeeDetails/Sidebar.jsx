0f296d5ba3221676a64c3fdbe70cc96f72cdbcb5
AdyC24 <adychandra101@gmail.com>
2024-08-25 14:19:18 +0800

    style[sidebar]: aktifkan warna sidebar yang sedang aktif

--- client/src/components/EmployeeDetails/Sidebar.jsx
+++ client/src/components/EmployeeDetails/Sidebar.jsx
@@ -1,23 +1,28 @@
 // Sidebar.jsx
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const Sidebar = () => {
     const { nik } =  useParams();
-    console.log(nik)
+    const location = useLocation();
 
+    const getLinkClass = (path) => {
+        return location.pathname.includes(path)
+            ? "bg-green-500 text-white"
+            : " hover:bg-green-500 hover:text-white transition duration-200"
+    }
     return (
         <div className="w-64 bg-green-600 shadow-lg flex flex-col h-full text-white mt-6 rounded-lg">
             <nav className="flex flex-col justify-center flex-grow py-4 space-y-4">
                 <Link
                     to={`/employee/${nik}/personal`}
-                    className="px-6 py-2 text-md hover:bg-green-500 hover:text-white transition duration-200"
+                    className={`px-6 py-2 text-md ${getLinkClass(`/employee/${nik}/personal`)}`}
                 >
                     Personal
                 </Link>
                 <Link
                     to={`/employee/${nik}/contract`}
-                    className="px-6 py-2 text-md hover:bg-green-500 hover:text-white transition duration-200"
+                    className={`px-6 py-2 text-md ${getLinkClass(`/employee/${nik}/contract`)}`}
                 >
                     Contract
                 </Link>
