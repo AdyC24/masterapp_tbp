f194290849bed3b3f1db93a3ed4f98195c58f318
AdyC24 <adychandra101@gmail.com>
2024-09-01 21:39:57 +0800

    style[outlet contract]: buat default fe untuk outlet contract

--- client/src/components/Contracts/Details/ComDev.jsx
+++ client/src/components/Contracts/Details/ComDev.jsx
@@ -1,67 +1,118 @@
 import React from "react";
 
 const ComDev = () => {
-    const contractData = [
-        {
-            contractName: "PKWT 1",
-            contractNo: "213/HRD/PKWT I/GPS/XI/2023",
-            startDate: "24 November 2023",
-            endDate: "23 May 2024",
-            status: "Not Active"
-        },
-        {
-            contractName: "PKWT 1 - Addendum 1",
-            contractNo: "354/HRD/PKWT I - Add I/GPS/II/2024",
-            startDate: "24 May 2024",
-            endDate: "23 August 2024",
-            status: "Not Active"
-        },
-        {
-            contractName: "PKWT 1 - Addendum 2",
-            contractNo: "684/HRD/PKWT I - Add II/GPS/VII/2024",
-            startDate: "24 August 2024",
-            endDate: "23 November 2024",
-            status: "Active"
-        }
-    ];
 
     return(
         <div>
-            <h2 className="text-2xl font-semibold mb-4">Contract Information</h2>
-            <div className="overflow-x-auto shadow-lg rounded-lg">
-                <table className="min-w-full bg-white border-collapse">
-                    <thead>
-                        <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
-                            <th className="py-3 px-6 text-left">Contract</th>
-                            <th className="py-3 px-6 text-left">Contract No</th>
-                            <th className="py-3 px-6 text-left">Start Date</th>
-                            <th className="py-3 px-6 text-left">End Date</th>
-                            <th className="py-3 px-6 text-left">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-gray-600 text-sm font-light">
-                         {contractData.map((contract, index) => (
-                            <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
-                                <td className="py-3 px-6 text-left whitespace-nowrap">
-                                    <span className="font-medium">{contract.contractName}</span>
-                                </td>
-                                <td className="py-3 px-6 text-left">{contract.contractNo}</td>
-                                <td className="py-3 px-6 text-left">{contract.startDate}</td>
-                                <td className="py-3 px-6 text-left">{contract.endDate}</td>
+            <div className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">Non Local</h2>
+                <div className="overflow-x-auto shadow-lg rounded-lg">
+                    <table className="min-w-full bg-white border-collapse">
+                        <thead>
+                            <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
+                                <th className="py-3 px-6 text-left">No</th>
+                                <th className="py-3 px-6 text-left">NIK</th>
+                                <th className="py-3 px-6 text-left">Name</th>
+                                <th className="py-3 px-6 text-left">Level</th>
+                                <th className="py-3 px-6 text-left">Position</th>
+                                <th className="py-3 px-6 text-left">Length of Service</th>
+                                <th className="py-3 px-6 text-left">Contract</th>
+                                <th className="py-3 px-6 text-left">Expired Date</th>
+                                <th className="py-3 px-6 text-left">PA</th>
+                                <th className="py-3 px-6 text-left">SP</th>
+                                <th className="py-3 px-6 text-left">Direct</th>
+                                <th className="py-3 px-6 text-left">Sign</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-gray-600 text-sm font-light">
+                            <tr className="border-b border-gray-200 hover:bg-gray-100">
+                                <td className="py-3 px-6 text-left">1</td>
+                                <td className="py-3 px-6 text-left">02D2300092</td>
+                                <td className="py-3 px-6 text-left">Budi Susanto</td>
+                                <td className="py-3 px-6 text-left">II</td>
+                                <td className="py-3 px-6 text-left">Health Staff</td>
+                                <td className="py-3 px-6 text-left">PKWT 1 - Add 1</td>
+                                <td className="py-3 px-6 text-left">8 months</td>
+                                <td className="py-3 px-6 text-left">14 September 2024</td>
                                 <td className="py-3 px-6 text-left">
-                                <span className={`py-1 px-3 rounded-full text-xs ${contract.status === "Active" ? "bg-green-200 text-green-600" : "bg-red-200 text-red-600"}`}>
-                                        {contract.status}
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-green-200 text-green-600">
+                                        Done
+                                    </span>
+                                </td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-green-200 text-green-600">
+                                        Done
+                                    </span>
+                                </td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-red-200 text-red-600">
+                                        Not Yet
+                                    </span>
+                                </td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-red-200 text-red-600">
+                                        Not Yet
                                     </span>
                                 </td>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div className="flex justify-center mt-8">
-                <button className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition duration-200">
-                    Add Contract
-                </button>
+            <div className="mb-8">
+                <h2 className="text-2xl font-semibold mb-4">Local</h2>
+                <div className="overflow-x-auto shadow-lg rounded-lg">
+                    <table className="min-w-full bg-white border-collapse">
+                        <thead>
+                            <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
+                                <th className="py-3 px-6 text-left">No</th>
+                                <th className="py-3 px-6 text-left">NIK</th>
+                                <th className="py-3 px-6 text-left">Name</th>
+                                <th className="py-3 px-6 text-left">Level</th>
+                                <th className="py-3 px-6 text-left">Position</th>
+                                <th className="py-3 px-6 text-left">Length of Service</th>
+                                <th className="py-3 px-6 text-left">Contract</th>
+                                <th className="py-3 px-6 text-left">Expired Date</th>
+                                <th className="py-3 px-6 text-left">PA</th>
+                                <th className="py-3 px-6 text-left">SP</th>
+                                <th className="py-3 px-6 text-left">Direct</th>
+                                <th className="py-3 px-6 text-left">Sign</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-gray-600 text-sm font-light">
+                            <tr className="border-b border-gray-200 hover:bg-gray-100">
+                                <td className="py-3 px-6 text-left">1</td>
+                                <td className="py-3 px-6 text-left">M0406240399</td>
+                                <td className="py-3 px-6 text-left">Shahbudin Abdullah</td>
+                                <td className="py-3 px-6 text-left">II</td>
+                                <td className="py-3 px-6 text-left">Health Staff</td>
+                                <td className="py-3 px-6 text-left">PKWT 1 - Add 1</td>
+                                <td className="py-3 px-6 text-left">5 months</td>
+                                <td className="py-3 px-6 text-left">14 September 2024</td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-green-200 text-green-600">
+                                        Done
+                                    </span>
+                                </td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-green-200 text-green-600">
+                                        Done
+                                    </span>
+                                </td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-green-200 text-green-600">
+                                        Done
+                                    </span>
+                                </td>
+                                <td className="py-3 px-6 text-left">
+                                    <span className= "py-1 px-3 rounded-full text-xs bg-red-200 text-red-600">
+                                        Not Yet
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>       
     )
