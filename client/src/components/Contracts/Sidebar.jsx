4653625917d8c8c2532480ba0d8b0cd7f672ff16
AdyC24 <adychandra101@gmail.com>
2024-09-01 19:19:05 +0800

    fix[sidebar]: ubah data dummy di sidebar contract

--- client/src/components/Contracts/Sidebar.jsx
+++ client/src/components/Contracts/Sidebar.jsx
@@ -3,27 +3,88 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    const dummyDept = [   
-        'Community Development',
-        'Environment & Business Improvement',
-        'Exploration',
-        'External Relation',
-        'Forestry Permitting And Compliance',
-        'Grade Control',
-        'HR & GA',
-        'Logistic',
-        'Mine Services',
-        'MPE - Engineering',
-        'MPE - Survey',
-        'MPE - Site Fluk',
-        'OHS & Training',
-        'Plant',
-        'Preparation & Laboratorium',
-        'Production',
-        'Quality Control',
-        'Security',
-        'Shipping - TBP & GTS',
-        'Shipping - GSP'
+    const dummyDept = [
+        {
+            picName: 'Community Development',
+            picNick: 'ComDev'
+        },
+        {
+            picName: 'Environment & Business Improvement',
+            picNick: 'EBI'
+        },
+        {
+            picName: 'Exploration',
+            picNick: 'Exp'
+        },
+        {
+            picName: 'External Relation',
+            picNick: 'ExRel'
+        },
+        {
+            picName: 'Forestry Permitting And Compliance',
+            picNick: 'Forestry'
+        },
+        {
+            picName: 'Grade Control',
+            picNick: 'GC'
+        },
+        {
+            picName: 'HR & GA',
+            picNick: 'HRGA'
+        },      
+        {
+            picName: 'Logistic',
+            picNick: 'Log'
+        },      
+        {
+            picName: 'Mine Services',
+            picNick: 'MSC'
+        },      
+        {
+            picName: 'MPE - Engineering',
+            picNick: 'MPE_Eng'
+        },      
+        {
+            picName: 'MPE - Survey',
+            picNick: 'MPE_Survey'
+        },      
+        {
+            picName: 'MPE - Site Fluk',
+            picNick: 'MPE_Fluk'
+        },      
+        {
+            picName: 'OHS & Training',
+            picNick: 'OHST'
+        },      
+        {
+            picName: 'Plant',
+            picNick: 'Plant'
+        },      
+        {
+            picName: 'Preparation & Laboratorium',
+            picNick: 'PrepLab'
+        },      
+        {
+            picName: 'Production',
+            picNick: 'Prod'
+        },      
+        {
+            picName: 'Quality Control',
+            picNick: 'QC'
+        },      
+        {
+            picName: 'Security',
+            picNick: 'Security'
+        },      
+        {
+            picName: 'Shipping - TBP & GTS',
+            picNick: 'Shipping_TBPGTS'
+        },      
+        {
+            picName: 'Shipping - GSP',
+            picNick: 'Shipping_GSP'
+        }
+        
     ]
 
     return (
@@ -31,11 +92,11 @@ const Sidebar = () => {
             <nav className="flex flex-col justify-center flex-grow py-4 space-y-4">
                 {dummyDept.map((dept, index) => (
                     <Link
-                        key={index}
-                        to={`/contracts/comdev`}
+                        key={dept.picName}
+                        to={`/contracts/${dept.picNick}`}
                         className={`px-6 py-2 text-md hover:bg-green-500 hover:text-white transition duration-200`}
                     >
-                        {dept}
+                        {dept.picName}
                     </Link>
                 ))}
             </nav>
