df18b43af858f07db3c868102e007a6e29fd975a
Ady Candra <adychandra101@gmail.com>
2024-11-28 15:53:31 +0900

    buat pagination & search di halaman contract

--- client/src/components/Contracts/ContractPage.jsx
+++ client/src/components/Contracts/ContractPage.jsx
@@ -6,6 +6,40 @@ import axios from "axios";
 
 const ContractPage = () => {
     const [contracts, setContracts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filterContracts = contracts.filter(contract => 
+        contract.compId.toLowerCase().includes(searchTerm.toLowerCase())
+        // contract.empNik.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        // contract.persName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    const totalPages = Math.ceil(filterContracts.lenght / itemsPerPage)
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentContracts = filterContracts.slice(indexOfFirstItem, indexOfLastItem)
+
+    console.log(currentContracts)
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage-1);
+        }
+    }
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.terget.value);
+        setCurrentPage(1);
+    }
 
     const fetchContract = useCallback(
         async () => {
@@ -20,7 +54,8 @@ const ContractPage = () => {
     useEffect(() => {
         fetchContract();
     }, [fetchContract]);
-        
+    
+    
 
     return (
         <div className="flex flex-col min-h-screen bg-gray-50">
@@ -32,6 +67,18 @@ const ContractPage = () => {
                         <h2 className="text-3xl font-semibold mb-8">Contracts</h2>
                         <div className="mb-8">
                             <div className="overflow-x-auto shadow-lg rounded-lg">
+
+                                {/* Search */}
+                                <div className="flex justify-end items-center mb-4">
+                                    <input 
+                                        type="text" 
+                                        placeholder="Cari karyawan..."
+                                        value={searchTerm}
+                                        onChange={handleSearch}
+                                        className="border border-gray-300 rounded-lg px-4 py-2 w-1/4"
+                                    />
+                                </div>
+
                                 <table className="min-w-full bg-white border-collapse">
                                     <thead>
                                         <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
@@ -48,8 +95,8 @@ const ContractPage = () => {
                                         </tr>
                                     </thead>
                                     <tbody className="text-gray-600 text-sm font-light">
-                                    {contracts.map(contract => (
-                                            <tr className="border-b border-gray-200 hover:bg-gray-100">
+                                    {currentContracts.map(contract => (
+                                            <tr className="border-b border-gray-200 hover:bg-gray-100"> 
                                                 <td className="py-3 px-6 text-left">nik</td>
                                                 <td className="py-3 px-6 text-left">nama</td>
                                                 <td className="py-3 px-6 text-left">level</td>
@@ -76,6 +123,27 @@ const ContractPage = () => {
                                         ))}
                                     </tbody>
                                 </table>
+
+                                {/* Pagination */}
+                                <div className="flex justify-between items-center mt-4">
+                                    <button
+                                        onClick={handlePreviousPage}
+                                        disabled={currentPage === 1}
+                                        className={`px-4 py-2 text-white ${currentPage === 1 ? 'bg-gray-400' : 'bg-green-600 hover:bg-green-500'}`}
+                                    >
+                                        Previous
+                                    </button>
+                                    <span className="text-gray-700">
+                                        Page {currentPage} of {totalPages}
+                                    </span>
+                                    <button
+                                        onClick={handleNextPage}
+                                        disable={currentPage === totalPages}
+                                        className={`px-4 py-2 text-white ${currentPage === totalPages ? 'bg-gray-400' : 'bg-green-600 hover:bg-green-500'}`}
+                                    >
+                                        Next
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div> 
