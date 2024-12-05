e11fb5a5ea45ed0465ea9a8ace1da2098bac9a69
Ady Candra <adychandra101@gmail.com>
2024-12-05 11:10:25 +0900

    [S2] Buat button add new employee di halaman daftar karyawan

--- client/src/components/Employee/EmployeeTable.jsx
+++ client/src/components/Employee/EmployeeTable.jsx
@@ -41,22 +41,22 @@ const EmployeeTable = ({ employees, handleRowClick }) => {
 
     return (
         <div className="overflow-x-auto">
-            <div className="flex items-center gap-2">
+            <div>
                 <Link to={"/home"} className="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="green" className="w-6 h-6">
                         <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
                     </svg>
-                    <p className="text-sm text-gray-700">Kembali</p>
+                    <p className="text-sm text-gray-700">Back</p>
                 </Link>
             </div>
-            <div className="pl-6 flex justify-between items-center mb-4">
-                <button className="">
-                        Tambah Karayawan
+            <div className="pl-6 flex justify-between items-center mb-4 mt-2">
+                <button className="bg-green-700 hover:bg-green-800 text-white font-medium py-2 px-4 rounded shadow-md">
+                    Add New
                 </button>
                 {/* Search Input */}
                 <input
                     type="text"
-                    placeholder="Cari karyawan..."
+                    placeholder="Search Employee..."
                     value={searchTerm}
                     onChange={handleSearch}
                     className="border border-gray-300 rounded-lg px-4 py-2 w-1/4" // Atur width menjadi lebih kecil
@@ -68,10 +68,10 @@ const EmployeeTable = ({ employees, handleRowClick }) => {
                 <thead className="bg-green-600 text-white">
                     <tr>
                         <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">NIK</th>
-                        <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Nama</th>
-                        <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Jabatan</th>
+                        <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Name</th>
+                        <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Position</th>
                         <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Department</th>
-                        <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Perusahaan</th>
+                        <th className="py-3 px-6 text-left font-semibold text-sm uppercase tracking-wider">Company</th>
                     </tr>
                 </thead>
                 <tbody>
