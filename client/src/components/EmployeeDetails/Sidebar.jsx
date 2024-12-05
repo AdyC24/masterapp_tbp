49b6b916991f821dfc63a538e7c3e0d743f11a87
Ady Candra <adychandra101@gmail.com>
2024-12-05 10:41:46 +0900

    [S2] Membuat halaman data personal karyawan

--- client/src/components/EmployeeDetails/Sidebar.jsx
+++ client/src/components/EmployeeDetails/Sidebar.jsx
@@ -15,6 +15,12 @@ const Sidebar = () => {
         <div className="w-64 bg-green-600 shadow-lg flex flex-col h-full text-white mt-6 rounded-lg">
             <nav className="flex flex-col justify-center flex-grow py-4 space-y-4">
                 <Link
+                    to={`/employee/${nik}/personal`}
+                    className={`px-6 py-2 text-md ${getLinkClass(`/employee/${nik}/personal`)}`}
+                >
+                    Personal
+                </Link>
+                <Link
                     to={`/employee/${nik}/contract`}
                     className={`px-6 py-2 text-md ${getLinkClass(`/employee/${nik}/contract`)}`}
                 >
