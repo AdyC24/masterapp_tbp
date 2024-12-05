a09e77966c7a636644762f5c7e4a409f1e70c960
Ady Candra <adychandra101@gmail.com>
2024-12-05 16:44:59 +0900

    [S2] Isi form dengan input sesuai dengan data kontrak yang dibutuhkan

--- client/src/components/Employee/EmployeeTable.jsx
+++ client/src/components/Employee/EmployeeTable.jsx
@@ -244,6 +244,17 @@ const EmployeeTable = ({ employees, handleRowClick }) => {
                                         placeholder="Enter Date of Hire"
                                     />
                                 </div>
+                                <div className="mb-4">
+                                    <label className="block text-gray-700 text-sm font-medium mb-2" htmlFor="phoneNumber">
+                                        Phone Number
+                                    </label>
+                                    <input 
+                                        type="text" 
+                                        id="phoneNumber"
+                                        className="w-full border border-gray-300 rounded px-3 py-2 text-gray-800"
+                                        placeholder="Enter Phone Number"
+                                    />
+                                </div>
                                 {/* Modal Action */}
                                 <div className="flex justify-end space-x-4">
                                     <button
