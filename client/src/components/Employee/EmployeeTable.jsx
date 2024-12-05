100dc02015a8cc138ddf34fe3a4828bef2419cb1
Ady Candra <adychandra101@gmail.com>
2024-12-05 10:54:29 +0900

    [S2] Buat button add new employee di halaman daftar karyawan

--- client/src/components/Employee/EmployeeTable.jsx
+++ client/src/components/Employee/EmployeeTable.jsx
@@ -41,16 +41,19 @@ const EmployeeTable = ({ employees, handleRowClick }) => {
 
     return (
         <div className="overflow-x-auto">
-            {/* Search Input */}
+            <div className="flex items-center gap-2">
+                <Link to={"/home"} className="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="green" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
+                    </svg>
+                    <p className="text-sm text-gray-700">Kembali</p>
+                </Link>
+            </div>
             <div className="pl-6 flex justify-between items-center mb-4">
-                <div className="flex items-center gap-2">
-                    <Link to={"/home"} className="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="green" className="w-6 h-6">
-                            <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
-                        </svg>
-                        <p className="text-sm text-gray-700">Back</p>
-                    </Link>
-                </div>
+                <button className="">
+                        Tambah Karayawan
+                </button>
+                {/* Search Input */}
                 <input
                     type="text"
                     placeholder="Cari karyawan..."
