e5482cf910af1c72c4ce44de774cf9beb16a5a5e
Ady Candra <adychandra101@gmail.com>
2025-01-29 08:37:12 +0900

    refactor EmployeeDetails component to improve navigation link structure

--- client/src/components/EmployeeDetails/EmployeeDetails.jsx
+++ client/src/components/EmployeeDetails/EmployeeDetails.jsx
@@ -13,12 +13,14 @@ const EmployeeDetails = () => {
             <div className="flex flex-grow">
                 <Sidebar />
                 <div className="flex-grow p-8 bg-white shadow-lg rounded-lg mx-4 my-6">
-                    <Link to={"/employee"} className="flex items-center mb-2">
+                    <div className="flex items-center mb-2">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="green" className="w-6 h-6">
                             <path fillRule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" />
                         </svg>
-                        <p className="text-sm text-gray-700">Back</p>
-                    </Link>
+                        <Link to={"/employee"} className="text-sm text-gray-700 ml-2">
+                            Back
+                        </Link>
+                    </div>
                     <Outlet />
                 </div>
             </div>
